0c20faf6df02ac7ecb95c6cc81c3ce0186d3602f
NamaKa298 <marion.saint-martin@protonmail.com>
2024-06-13 10:23:02 +0200

    sque tu veux

--- API/v1/country.py
+++ API/v1/country.py
@@ -1,46 +1,23 @@
 from flask import request, jsonify
 from API.v1.app import app
-from Persistence.datamanager import data_manager as Countries_repository
+from Persistence.datamanager import data_manager as country_repository
 
-@app.route('/test', methods=['GET'])
-def test():
-    return jsonify({"message": "Hello World"}), 200
+@app.route('/countries', methods=['GET'])
+def read_countries():
+    countries = country_repository.all("Country")
+    return jsonify([countries[id].to_dict() for id in countries]), 200
 
-@app.route('/countries', methods=['POST'])
-def create_countrie():
-    from Model.country import Country
-    data = request.get_json()
-    Country = Country(**data)
-    Countries_repository.save(Country)
-    return jsonify(Country.to_dict()), 201
-
-@app.route('/Countries', methods=['GET'])
-def read_Countries():
-    Countries = Countries_repository.all("Countries")
-    return jsonify([Country.to_dict() for Country in Countries]), 200
-
-@app.route('/Countries/<id>', methods=['GET'])
-def read_Country(email):
-    Country = Countries_repository.find_by_email(email)
-    if Country is None:
+@app.route('/countries/<id>', methods=['GET'])
+def read_country(id):
+    country = country_repository.get(id, "Country")
+    if country is None:
         return jsonify({"error": "Country not found"}), 404
-    return jsonify(Country.to_dict()), 200
+    return jsonify(country.to_dict()), 200
 
-@app.route('/Countries/<id>', methods=['PUT'])
-def update_Country(email):
-    Country = Countries_repository.find_by_email(email)
-    if Country is None:
+@app.route('/countries/<id>/cities', methods=['GET'])
+def read_country_cities(id):
+    country = country_repository.get(id, "Country")
+    if country is None:
         return jsonify({"error": "Country not found"}), 404
-    data = request.get_json()
-    Country.update(data)
-    Countries_repository.save(Country)
-    return jsonify(Country.to_dict()), 200
-
-@app.route('/Countries/<id>', methods=['DELETE'])
-def delete_Country(email):
-    Country = Countries_repository.find_by_email(email)
-    if Country is None:
-        return jsonify({"error": "Country not found"}), 404
-    Countries_repository.delete(Country)
-    return jsonify({}), 204
-
+    cities = country.cities
+    return jsonify([city.to_dict() for city in cities]), 200
